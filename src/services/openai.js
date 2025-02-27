598caf86bbe3b393ad933a9e4b3a9ec6887a89ef
Lipe biondes <alefefelipebiondesmagalhaes@gmail.com>
2025-02-27 10:48:05 -0300

    fix: corrige configuracao da openAi

--- src/services/openai.js
+++ src/services/openai.js
@@ -1,24 +1,25 @@
-const openAiRequire = require('openai')
-
+const { OpenAIApi, Configuration } = require('openai')
 const dotenv = require('dotenv')
 
 dotenv.config()
 
-const openai = new openAiRequire.OpenAI({
+const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY
 })
 
-const sanitizeText = async text => {
+const openai = new OpenAIApi(configuration)
+
+const sanitizeText = async (text) => {
   const prompt = `
-  Sanitizar o seguinte texto substituindo abreviações comuns por suas formas completas, convertendo tudo para letras minúsculas, removendo pontuações, artigos definidos e indefinidos, stop words, remova numeros, quebra de linha, subistitua os verbos para sua forma no infinitivo ou para a sua forma nominal e retorne o texto como uma frase. Aqui está o texto: "${text}"
+  Sanitizar o seguinte texto substituindo abreviações comuns por suas formas completas, convertendo tudo para letras minúsculas, removendo pontuações, artigos definidos e indefinidos, stop words, remova números, quebras de linha, substitua os verbos para sua forma no infinitivo ou para a sua forma nominal e retorne o texto como uma frase. Aqui está o texto: "${text}"
   `
 
   try {
-    const completion = await openai.chat.completions.create({
+    const completion = await openai.createChatCompletion({
       model: 'gpt-3.5-turbo',
       messages: [{ role: 'user', content: prompt }]
     })
-    return completion.choices[0].message.content
+    return completion.data.choices[0].message.content
   } catch (error) {
     console.log(`Error getting OpenAI completion: ${error}`)
     return 'Não foi possível completar a mensagem. Tente novamente mais tarde.'
